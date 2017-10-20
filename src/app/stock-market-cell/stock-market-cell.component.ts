26a1a1ed6e1f66fbc3999e68ba9d71f1d806888d
James Shore <jshore@jamesshore.com>
2017-10-19 18:18:09 -0700

    Start experimenting with dynamic components

--- src/app/stock-market-cell/stock-market-cell.component.ts
+++ src/app/stock-market-cell/stock-market-cell.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockMarketCellComponent implements OnInit {
 
-  constructor() { }
+  value = "Hi!";
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
