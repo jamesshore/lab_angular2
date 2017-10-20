537833fe5d367ff7a04c73767612d1d19499566a
James Shore <jshore@jamesshore.com>
2017-10-19 18:50:31 -0700

    Finish converting year.js to TypeScript

---
+++ src/values/__render_target_stub.ts
@@ -0,0 +1,12 @@
+// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+"use strict";
+
+export class RenderTargetStub {
+
+  public rendering: any;
+
+  render(values: any): void {
+    this.rendering = values;
+  }
+
+}
--- src/values/year.spec.ts
+++ src/values/year.spec.ts
@@ -1,32 +1,32 @@
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 "use strict";
 
-var Year = require("./year.js");
-var __RenderTargetStub = require("./__render_target_stub.js");
+import { Year } from "./year";
+import { RenderTargetStub } from "./__render_target_stub";
 
 describe("Year", function() {
 
-	var _2010 = new Year(2010);
-	var _2011 = new Year(2011);
-	var _2050 = new Year(2050);
+	const _2010 = new Year(2010);
+	const _2011 = new Year(2011);
+	const _2050 = new Year(2050);
 
 	it("increments year", function() {
-		expect(_2010.nextYear()).to.eql(_2011);
+		expect(_2010.nextYear()).toEqual(_2011);
 	});
 
 	it("counts years in range", function() {
-		expect(_2010.numberOfYearsInclusive(_2050)).to.equal(41);
+		expect(_2010.numberOfYearsInclusive(_2050)).toEqual(41);
 	});
 
 	it("converts to string", function() {
-		expect(_2010.toString()).to.equal("2010");
+		expect(_2010.toString()).toEqual("2010");
 	});
 
 	it("renders itself", function() {
-		var target = new __RenderTargetStub();
+		const target = new RenderTargetStub();
 		_2010.renderTo(target);
 
-		expect(target.rendering).to.eql({
+		expect(target.rendering).toEqual({
 			text: "2010",
 			negative: false,
 			invalid: false
