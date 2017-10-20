a7cab46f1d3f09042317db298eaff5d06b9c2ce7
James Shore <jshore@jamesshore.com>
2017-10-19 18:21:39 -0700

    Use inline template for StockMarketCell component

--- src/app/stock-market-cell/stock-market-cell.component.ts
+++ src/app/stock-market-cell/stock-market-cell.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-stock-market-cell]',
-  templateUrl: './stock-market-cell.component.html',
+  template: '{{value}}',
   styleUrls: ['./stock-market-cell.component.css']
 })
 export class StockMarketCellComponent implements OnInit {
